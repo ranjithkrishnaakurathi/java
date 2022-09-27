d3e8f55e70a0a6a7253ee1452579f5648507eecf
ranjithkrishnaakurathi <113251275+ranjithkrishnaakurathi@users.noreply.github.com>
2022-09-27 18:24:40 +0530

    Add files via upload

---
+++ SumTwoNumbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class SumTwoNumbers{
+		public static void main(String[] args){
+			Scanner s = new Scanner(System.in);
+			int a,b,c;
+			System.out.println("Enter two numbers: ");
+			a=s.nextInt();
+			b=s.nextInt();
+			c=a+b;
+			System.out.println("Sum is: "+c);
+	}
+}
