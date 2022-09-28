19990e1d2553e923bc4d52dcef3393b7f6b683df
ranjithkrishnaakurathi <113251275+ranjithkrishnaakurathi@users.noreply.github.com>
2022-09-28 16:11:46 +0530

    Add files via upload

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,31 @@
+class PrimitiveDataTypes
+{
+	public static void main(String[] args)
+	{
+		byte b = 123;
+		System.out.println("Byte: "+b);
+		
+		short s = 1234;
+		System.out.println("Short: "+s);
+		
+		int i = 12345;
+		System.out.println("Int: "+i);
+		
+		long l = 1234567;
+		System.out.println("Long: "+l);
+		
+		float f = 123.45f;
+		System.out.println("Float: "+f);
+		
+		double d = 123.456789;
+		System.out.println("Double: "+d);
+		
+		char ch = 'R';
+		System.out.println("Char: "+ch);
+		
+		boolean bool = true;
+		System.out.println("Boolean: "+bool);
+		
+	}
+}
+		
