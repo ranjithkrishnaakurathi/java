b35ff2d5f9348274211fbdda334b011511d99fa6
ranjithkrishnaakurathi <113251275+ranjithkrishnaakurathi@users.noreply.github.com>
2022-09-30 21:14:43 +0530

    Add files via upload

---
+++ SimpleIf.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class SimpleIf
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter age : ");
+		int age = sc.nextInt();
+		if (age > 18)
+		{
+			System.out.println("Candidate is eligible for vote...");
+		}
+	}
+}
