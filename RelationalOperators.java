283e9caf232c3f905568707ef15816e9f01ff6cf
ranjithkrishnaakurathi <113251275+ranjithkrishnaakurathi@users.noreply.github.com>
2022-09-30 20:38:20 +0530

    Add files via upload

---
+++ RelationalOperators.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class RelationalOperators
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter num1 value: ");
+		int num1 = sc.nextInt();
+		System.out.println("Enter num2 value: ");
+		int num2 = sc.nextInt();
+		System.out.println("num1 == num2: "+(num1 == num2));
+		System.out.println("num1 != num2: "+(num1 != num2));
+		System.out.println("num1 > num2: "+(num1 > num2));
+		System.out.println("num1 >= num2: "+(num1 >= num2));
+		System.out.println("num1 < num2: "+(num1 < num2));
+		System.out.println("num1 <= num2: "+(num1 <= num2));
+	
+	}
+}
