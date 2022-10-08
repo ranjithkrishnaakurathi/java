ff73d5df5a8cd7e038da48fd4e27a9def7336139
ranjithkrishnaakurathi <113251275+ranjithkrishnaakurathi@users.noreply.github.com>
2022-10-08 16:24:28 +0530

    Add files via upload

---
+++ StringReverse1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class StringReverse1
+{
+	public static void main(String[] args)
+	{
+		String name = "ABCDE";
+		char[] nameArray = name.toCharArray();
+		
+		int start = 0;
+		int end = nameArray.length-1;
+		
+		while(start <= end){
+			char temp = nameArray[start];
+			nameArray[start] = nameArray[end];
+			nameArray[end] = temp;
+			start++;
+			end--;
+		}
+		
+		for (char c : nameArray){
+			System.out.print(c);
+		}
+	}
+}
+
