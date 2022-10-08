d67fe285ae1bb7c37601ef77cf58d375b715c3aa
ranjithkrishnaakurathi <113251275+ranjithkrishnaakurathi@users.noreply.github.com>
2022-10-08 20:49:35 +0530

    Add files via upload

---
+++ ArraySort.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class ArraySort
+{
+	public static void main(String[] args)
+	{	
+		int[] a = {1,10,2,5,3};
+		Arrays.sort(a);
+		for(int i : a)
+		{
+			System.out.print(i+" ");
+		}
+	}
+}
+
+	
