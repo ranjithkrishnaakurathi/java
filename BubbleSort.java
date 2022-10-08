6723a4facfde3f8f96ad61b3dce4d46553793ddc
ranjithkrishnaakurathi <113251275+ranjithkrishnaakurathi@users.noreply.github.com>
2022-10-08 16:25:07 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class BubbleSort
+{
+	public static void main(String[] args)
+	{	
+		int[] a = {5,2,3,7,6,1,9};
+	
+		for(int i = 0;i < a.length;i++)
+		{
+			for(int j = 1;j <a.length-i;j++)
+			{
+				if(a[j-1] >= a[j])
+				{
+					int temp = a[j-1];
+					a[j-1] = a[j];
+					a[j] = temp;
+				}	
+			}
+		}
+		for(int i : a)
+		{
+			System.out.print(i+" ");
+		}
+	}
+}
+
+	
