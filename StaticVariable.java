2b225d91adbc7551239f14dbffa1b69e524118bd
ranjithkrishnaakurathi <113251275+ranjithkrishnaakurathi@users.noreply.github.com>
2022-10-09 12:18:27 +0530

    Add files via upload

---
+++ StaticVariable.java
@@ -0,0 +1,45 @@
+class StaticVariable
+{	
+	int id;
+	String name;
+	static String dept="ECE";
+	public static void main(String[] args)
+	{	
+		StaticVariable obj = new StaticVariable();
+		obj.id=101;
+		obj.name="Ranjith";
+		System.out.println("-------------------------");
+		System.out.println("Student1 details: ");
+		System.out.println("id: "+obj.id);
+		System.out.println("name: "+obj.name);
+		System.out.println("dept: "+obj.dept);
+		
+		StaticVariable obj1 = new StaticVariable();
+		obj1.id=201;
+		obj1.name="Krishna";
+		System.out.println("-------------------------");
+		System.out.println("Student2 details: ");
+		System.out.println("id: "+obj1.id);
+		System.out.println("name: "+obj1.name);
+		System.out.println("dept: "+obj1.dept);
+		
+		obj1.dept="CSE";
+		
+		System.out.println("After Update: ");
+		
+		System.out.println("-------------------------");
+		System.out.println("Student1 details: ");
+		System.out.println("id: "+obj.id);
+		System.out.println("name: "+obj.name);
+		System.out.println("dept: "+obj.dept);
+		
+		System.out.println("-------------------------");
+		System.out.println("Student2 details: ");
+		System.out.println("id: "+obj1.id);
+		System.out.println("name: "+obj1.name);
+		System.out.println("dept: "+obj1.dept);
+		
+	}
+}
+
+	
