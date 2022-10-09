d90bb8813d8980663d785eee01533034d0f1407a
ranjithkrishnaakurathi <113251275+ranjithkrishnaakurathi@users.noreply.github.com>
2022-10-09 09:40:09 +0530

    Add files via upload

---
+++ VarArgs.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+class VarArgs
+{	
+	static int sum(int... x)
+	{
+		int sum = 0;
+		for(int i = 0;i < x.length;i++)
+		{
+			sum = sum + x[i];
+		}
+		return sum;
+	}
+	public static void main(String[] args)
+	{
+		int result = sum(10,20);
+		System.out.println(result);
+		
+		int result1 = sum(10,20,30);
+		System.out.println(result1);
+		
+		int result2 = sum(10,20,30,40);
+		System.out.println(result2);
+	}
+}
+
+	
