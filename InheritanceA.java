0143bc4aab3fc12f96b961316c09e9c096752b7f
ranjithkrishnaakurathi <113251275+ranjithkrishnaakurathi@users.noreply.github.com>
2022-10-15 18:53:47 +0530

    Add files via upload

---
+++ InheritanceA.java
@@ -0,0 +1,19 @@
+package inheritance;
+
+public class InheritanceA {
+	
+		int x = 100;
+		
+		int y = 200;
+		
+		void m1()
+		{
+			System.out.println("A class m1 method");
+		}
+		
+		void m2()
+		{
+			System.out.println("A class m2 method");
+		}
+}
+
